22b2099f7ded16225f20d6a336e3e53cefb80bd7
petkoche <petkoche90@gmail.com>
2017-07-07 14:42:30 +0300

    pug working with local db and each

--- routers/server-routes.js
+++ routers/server-routes.js
@@ -1,11 +1,18 @@
-const { Router } = require('express');
+const {
+    Router,
+} = require('express');
 
 const attach = (app, data) => {
     // const homeController = require('../controllers/home-controller');
     const router = new Router();
 
     router.get('/', (req, res) => {
-        res.render('home');
+        data.getAllPosts()
+            .then((posts) => {
+                res.render('home', {
+                    'posts': posts,
+                });
+            });
     });
 
     app.use('/', router);
