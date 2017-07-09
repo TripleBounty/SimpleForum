0a5095bf80b0f37010553c189783539d4dddb261
petkoche <petkoche90@gmail.com>
2017-07-09 22:28:10 +0300

    likes and dislikes

--- static/js/post-like-handler.js
+++ static/js/post-like-handler.js
@@ -1,4 +1,5 @@
 /* globals $ */
+const changeingValue = 1;
 
 $('.likesLink').on('click', (event) => {
     event.preventDefault(); // Stop the form from causing a page refresh.
@@ -6,17 +7,37 @@ $('.likesLink').on('click', (event) => {
     const elemntToChange = 'positive' + postId;
     const targetDiv = document.getElementById(elemntToChange);
     const currentVal = parseFloat(targetDiv.innerHTML);
-
     $.ajax({
         url: '/vote',
         data: {
             'postId': postId,
-            'node': 1,
+            'node': changeingValue,
         },
         method: 'POST',
     }).then((response) => {
-        targetDiv.innerHTML = currentVal + 1;
+        targetDiv.innerHTML = currentVal + changeingValue;
         document.getElementById(postId).classList.add('inactive');
+    }).catch((err) => {
+        console.error(err);
+    });
+});
+
+$('.hatesLink').on('click', (event) => {
+    event.preventDefault(); // Stop the form from causing a page refresh.
+    const postId = event.target.id;
+    const elemntToChange = 'positive' + postId;
+    const targetDiv = document.getElementById(elemntToChange);
+    const currentVal = parseFloat(targetDiv.innerHTML);
+    $.ajax({
+        url: '/vote',
+        data: {
+            'postId': postId,
+            'node': -changeingValue,
+        },
+        method: 'POST',
+    }).then((response) => {
+        targetDiv.innerHTML = currentVal - changeingValue;
+        event.target.classList.add('inactive');
     }).catch((err) => {
         console.error(err);
     });
