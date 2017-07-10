740104916aea03a0ebaee0b7fb6f2f9b8bf8b21b
petkoche <petkoche90@gmail.com>
2017-07-10 21:26:13 +0300

    finally made the the likes work, no cookie validation, forum comment form grounds

--- static/js/post-like-handler.js
+++ static/js/post-like-handler.js
@@ -16,7 +16,12 @@ $('.likesLink').on('click', (event) => {
         method: 'POST',
     }).then((response) => {
         targetDiv.innerHTML = currentVal + changeingValue;
-        document.getElementById(postId).classList.add('inactive');
+        const negativeLike = document.getElementById(postId+'d');
+        if (negativeLike.classList.contains('inactive')) {
+            negativeLike.classList.remove('inactive');
+        } else {
+            document.getElementById(postId).classList.add('inactive');
+        }
     }).catch((err) => {
         console.error(err);
     });
@@ -24,7 +29,8 @@ $('.likesLink').on('click', (event) => {
 
 $('.hatesLink').on('click', (event) => {
     event.preventDefault(); // Stop the form from causing a page refresh.
-    const postId = event.target.id;
+    let postId = event.target.id;
+    postId = +(postId.substring(0, postId.length - 1));
     const elemntToChange = 'positive' + postId;
     const targetDiv = document.getElementById(elemntToChange);
     const currentVal = parseFloat(targetDiv.innerHTML);
@@ -37,7 +43,12 @@ $('.hatesLink').on('click', (event) => {
         method: 'POST',
     }).then((response) => {
         targetDiv.innerHTML = currentVal - changeingValue;
-        event.target.classList.add('inactive');
+        const positiveLike = document.getElementById(postId);
+        if (positiveLike.classList.contains('inactive')) {
+            positiveLike.classList.remove('inactive');
+        } else {
+            event.target.classList.add('inactive');
+        }
     }).catch((err) => {
         console.error(err);
     });
