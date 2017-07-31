575d17b4d923dacc4987ce45f669f9e4fabaafb3
Krasimir Tsvetanov <kvtsvetanov@gmail.com>
2017-07-31 09:10:05 +0300

    finaly finish first selenium test

--- gulpfile.js
+++ gulpfile.js
@@ -42,3 +42,45 @@ gulp.task('tests:unit', ['pre-test'], () => {
             process.exit();
         });
 });
+
+gulp.task('test-server-start', () => {
+    const config = require('./app/config/env-configs/env-configs');
+    const bucketConfig = require('./app/config/env-configs/bucket-config');
+    const port = config.port;
+
+    return Promise.resolve()
+        .then(() => require('./db')(config.connectionStringTest))
+        .then((db) => require('./data')(db))
+        .then((data) => require('./app/index')(data, bucketConfig))
+        .then((app) => {
+            return Promise.resolve(app.listen(port, () => console.log(`Server is running at http://localhost:${port}`)));
+        })
+        .then((server) => require('./app/config/socket-io/socket-io')(server));
+});
+
+const { MongoClient } = require('mongodb');
+const country = {
+    'name': 'Bulgaria',
+    'code': 'BG',
+};
+gulp.task('test-server:stop', () => {
+    const config = require('./app/config/env-configs/env-configs');
+    return MongoClient.connect(config.connectionStringTest)
+        .then((db) => {
+            return db.dropDatabase()
+                .then(() => {
+                    return db.collection('countries').insert(country);
+                });
+        });
+});
+
+
+gulp.task('tests:browser', ['test-server-start'], () => {
+    return gulp.src(['./tests/browser/tests/**/*.js'])
+        .pipe(mocha({
+            timeout: 30000,
+        }))
+        .once('end', () => {
+            gulp.start('test-server:stop');
+        });
+});
---
+++ tests/browser/tests/test.js
@@ -0,0 +1,103 @@
+const { expect } = require('chai');
+const { setupDriver } = require('../utils/setup-driver');
+const utils = require('../utils/ui');
+
+describe('Test', () => {
+    const user = {
+        firstName: 'firstname',
+        lastName: 'lastname',
+        date: '05-05-2017',
+        username: 'stest',
+        password: 'aabbaabb',
+        email: 'aa@abv.bg',
+        number: '+355888889999',
+    };
+
+
+    const appUrl = 'http://localhost:3000';
+    let driver = null;
+    beforeEach(() => {
+        driver = setupDriver('chrome');
+        utils.setDriver(driver);
+    });
+
+    it('expect Register to create user in database and loggin', (done) => {
+        driver.get(appUrl)
+            .then(() => {
+                return utils.click('#register');
+            })
+            .then(() => {
+                return utils.setValue('#inputFirstName', user.firstName);
+            })
+            .then(() => {
+                return utils.setValue('#inputLastName', user.lastName);
+            })
+            .then(() => {
+                return utils.setValue('#inputDate', user.date);
+            })
+            .then(() => {
+                return utils.setValue('#inputUserName', user.username);
+            })
+            .then(() => {
+                return utils.click('#countries > option:nth-child(2)');
+            })
+            .then(() => {
+                return utils.setValue('#inputPassword', user.password);
+            })
+            .then(() => {
+                return utils.setValue('#inputConfirmPassword', user.password);
+            })
+            .then(() => {
+                return utils.setValue('#inputEmail', user.email);
+            })
+            .then(() => {
+                return utils.setValue('#inputMobile', user.number);
+            })
+            .then(() => {
+                // eslint-disable-next-line max-len
+                return utils.click('#contact_form > fieldset > div.form-group > div > button');
+            })
+            .then(() => {
+                // eslint-disable-next-line max-len
+                return utils.click('#user-name-nav > a');
+            })
+            .then(() => {
+                return utils.getText('#user_name');
+            })
+            .then((prop) => {
+                expect(prop).to.be.deep.equal(user.username);
+            })
+            .then(() => {
+                return utils.getText('#first_name');
+            })
+            .then((prop) => {
+                expect(prop).to.be.deep.equal(user.firstName);
+            })
+            .then(() => {
+                return utils.getText('#last_name');
+            })
+            .then((prop) => {
+                expect(prop).to.be.deep.equal(user.lastName);
+            })
+            .then(() => {
+                return utils.getText('#email');
+            })
+            .then((prop) => {
+                expect(prop).to.be.deep.equal(user.email);
+            })
+            .then(() => {
+                return utils.getText('#number');
+            })
+            .then((prop) => {
+                expect(prop).to.be.deep.equal(user.number);
+            })
+            .then(() => {
+                return utils.getText('#country');
+            })
+            .then((prop) => {
+                expect(prop).to.be.deep.equal('Bulgaria');
+                done();
+            })
+            .catch(done);
+    });
+});
